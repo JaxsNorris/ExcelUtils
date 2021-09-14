cec4d15a52f6c3e87ef885ec278519ec7969a4d1
gitName <a@b.c>
2021-09-14 14:36:09 +0200

    update generator with new file

--- ConsoleApp/Runners/GeneratorRunner.cs
+++ ConsoleApp/Runners/GeneratorRunner.cs
@@ -6,6 +6,7 @@ using ConsoleApp.Utils;
 using Importer;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ConsoleApp.Runners
@@ -18,14 +19,17 @@ namespace ConsoleApp.Runners
         public GeneratorRunner(IDataGenerator dataGenerator)
         {
             _dataGenerator = dataGenerator;
-            _columnSetup = CreateDefaultColumnSetup();
+            _columnSetup = CreateMemberDataColumnSetup();
         }
 
         public async Task Run()
         {
-            await GenerateData("Small", 5000);
+            await GenerateData("Tiny", 5);
+            /*
+             * await GenerateData("Small", 5000);
             await GenerateData("Medium", 30000);
             await GenerateData("Big", 500000);
+            */
             Console.WriteLine("Finished saving file");
         }
 
@@ -37,6 +41,27 @@ namespace ConsoleApp.Runners
                 Console.WriteLine($"Generating {keyword} worksheet....");
                 await _dataGenerator.GenerateData(fullpath, "data", numberOfItems, _columnSetup);
             }
+        }
+
+        private List<ColumnDataGenerationOption> CreateMemberDataColumnSetup()
+        {
+            return new List<ColumnDataGenerationOption>()
+            {
+                new ColumnDataGenerationOption(){ CoulmnHeader = "Fullname", ColumnFieldType = FieldTypes.Text, HasBlankRows = true},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "cat", ColumnFieldType = FieldTypes.Text, Values =new[] { "cat1", "cat2" } },
+                new ColumnDataGenerationOption(){ CoulmnHeader = "Gender", ColumnFieldType = FieldTypes.Text, Values = GetGenderLookupValues()},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "Date of birth", ColumnFieldType = FieldTypes.DateTime},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "Salary", ColumnFieldType = FieldTypes.Number, HasBlankRows = true},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "Expat", ColumnFieldType = FieldTypes.Text, Values = GetBooleanLookupValues()},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "Disabled", ColumnFieldType = FieldTypes.Text, Values = GetBooleanLookupValues()},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "Date of disability", ColumnFieldType = FieldTypes.DateTime},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "country", ColumnFieldType = FieldTypes.Text, Values = GetLocationLookupValues()},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "Region", ColumnFieldType = FieldTypes.Text, Values = GetRegionLookupValues()},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "Industry", ColumnFieldType = FieldTypes.Text, Values = GetIndustyLookupValues()},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "CTA", ColumnFieldType = FieldTypes.Text, Values= GetMinMaxValues()},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "multiple", ColumnFieldType = FieldTypes.Number, Values= GetMinMaxValues(2,5)},
+                new ColumnDataGenerationOption(){ CoulmnHeader = "pec", ColumnFieldType = FieldTypes.Number, Values = new []{ "0.1", "0.2", "0.5" } },
+            };
         }
 
         private List<ColumnDataGenerationOption> CreateDefaultColumnSetup()
@@ -62,6 +87,53 @@ namespace ConsoleApp.Runners
                 new ColumnDataGenerationOption(){ CoulmnHeader = "Number Date Bool", ColumnFieldType = FieldTypes.Text, OtherFieldTypes = new FieldTypes[]{FieldTypes.Number,FieldTypes.DateTime, FieldTypes.Boolean } },
                 new ColumnDataGenerationOption(){ CoulmnHeader = "Date Bool", ColumnFieldType = FieldTypes.Text, OtherFieldTypes = new FieldTypes[]{FieldTypes.Boolean,FieldTypes.DateTime } },
             };
+        }
+
+        private string[] GetMinMaxValues(int start = 60, int end = 70)
+        {
+            return Enumerable.Range(start, end).Select(x => x.ToString()).ToArray();
+        }
+
+        private string[] GetLocationLookupValues()
+        {
+            return new[] {
+                "ZAF",
+                "KEN",
+                "LSO",
+            };
+        }
+
+        private string[] GetRegionLookupValues()
+        {
+            return new[] {
+                "EC",
+                "FS",
+                "GP",
+                "KZN",
+                "LP",
+                "MP",
+                "NW",
+                "NC",
+                "WC",
+            };
+        }
+
+        private string[] GetIndustyLookupValues()
+        {
+            return new[] {
+                "Professional",
+                "Administration",
+                "Retail & Wholesale Companies",
+                "D",
+                "E",
+                "F",
+                "G"
+            };
+        }
+
+        private string[] GetGenderLookupValues()
+        {
+            return new[] { "F", "M" };
         }
 
         private string[] GetLookupValues(int number)
